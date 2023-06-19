5a4ebd12eed84cbbc768a81a8602df9d0b6c1485
Andrei Merlescu <andrei+github@merlescu.net>
2023-06-19 13:04:40 -0400

    Implemented CountableWaitGroup

---
+++ countable-waitgroup.go
@@ -0,0 +1,49 @@
+package countable_waitgroup
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+type CountableWaitGroup struct {
+	wg      sync.WaitGroup
+	cnt     int64
+	stopped atomic.Bool
+}
+
+type IWaitGroup interface {
+	Add(i int)
+	Done()
+	Count() int64
+	IsPending() bool
+	PreventAdd()
+}
+
+func (cwg *CountableWaitGroup) Add(i int) {
+	if cwg.stopped.Load() == true {
+		return
+	}
+	atomic.AddInt64(&cwg.cnt, int64(i))
+	cwg.wg.Add(i)
+}
+
+func (cwg *CountableWaitGroup) Done() {
+	atomic.AddInt64(&cwg.cnt, -1)
+	cwg.wg.Done()
+}
+
+func (cwg *CountableWaitGroup) Count() int64 {
+	return atomic.LoadInt64(&cwg.cnt)
+}
+
+func (cwg *CountableWaitGroup) Wait() {
+	cwg.wg.Wait()
+}
+
+func (cwg *CountableWaitGroup) IsPending() bool {
+	return atomic.LoadInt64(&cwg.cnt) > 0
+}
+
+func (cwg *CountableWaitGroup) PreventAdd() {
+	cwg.stopped.Store(true)
+}
